80ec56ed67b9cf23acf269c714854031db7958ae
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2025-08-04 12:46:47 -0300

    Adiciona desabilitação de regra ESLint para permitir o uso de tipos explícitos em TypeScript no componente modalAdicionarStaff, facilitando a implementação de funcionalidades futuras.

--- components/operador/modalAdicionarStaff.tsx
+++ components/operador/modalAdicionarStaff.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
